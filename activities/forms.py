7ddbe17c8963112613d74fa583853833225ebf11
Jose Luis <jlorteg@gmail.com>
2024-04-29 21:56:50 -0600

    video_link test

--- activities/forms.py
+++ activities/forms.py
@@ -4,7 +4,7 @@ from .models import Activities
 class ActivitiesForm(forms.ModelForm):
     class Meta:
         model = Activities
-        fields = ['image', 'description']
+        fields = ['image','video_link', 'description']
         widgets = {
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a description'})
