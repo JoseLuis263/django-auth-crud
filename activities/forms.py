586be2486b40b151ded058dde42c9cbcb89da18b
Jose Luis <jlorteg@gmail.com>
2024-04-30 20:12:38 -0600

    models update

--- activities/forms.py
+++ activities/forms.py
@@ -4,9 +4,9 @@ from .models import Activities
 class ActivitiesForm(forms.ModelForm):
     class Meta:
         model = Activities
-        fields = ['description']
+        fields = ['description', 'image']
         widgets = {
-            #'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a description'})
             
         }
--- activities/models.py
+++ activities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Activities(models.Model):
-    #image = models.ImageField(null=True, blank=True, upload_to='image/')
+    image = models.ImageField(null=True, blank=True, upload_to='image/')
     #video_link = models.URLField(blank=True, max_length=200)
     description = models.TextField(blank=False)
     
