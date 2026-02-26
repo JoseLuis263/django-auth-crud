65b3d725bdb7d72848195c30bdb5adff52053997
Soy luis <soyluis@gmail.com>
2026-02-26 02:43:21 -0600

    add database

--- django_crud_auth/settings.py
+++ django_crud_auth/settings.py
@@ -80,12 +80,15 @@ WSGI_APPLICATION = 'django_crud_auth.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/postgres',
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
