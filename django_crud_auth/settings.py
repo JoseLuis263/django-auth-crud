6a5c1c4bba85fbe858d071224f3eb0bbab69f961
Luis <jlortegap@utbispuebla.edu.mx>
2026-03-10 19:50:29 -0600

    Update ALLOWED_HOSTS for production settings

--- django_crud_auth/settings.py
+++ django_crud_auth/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-!4=2=au8yyx5b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.80.31.148', 'localhost', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
